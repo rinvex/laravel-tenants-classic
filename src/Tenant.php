3d640e1c91defc48fd00aecbfdd2dca164856ae1
Abdelrahman Omran <me@omranic.com>
2017-04-27 15:31:13 +0300

    Add model class docblock

--- src/Tenant.php
+++ src/Tenant.php
@@ -15,6 +15,59 @@ use Illuminate\Database\Eloquent\Builder;
 use Spatie\EloquentSortable\SortableTrait;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
+/**
+ * Rinvex\Tenantable\Tenant
+ *
+ * @property int                            $id
+ * @property string                         $slug
+ * @property array                          $name
+ * @property array                          $description
+ * @property int                            $owner_id
+ * @property string                         $email
+ * @property bool                           $email_verified
+ * @property string                         $phone
+ * @property bool                           $phone_verified
+ * @property string                         $language_code
+ * @property string                         $country_code
+ * @property string                         $state
+ * @property string                         $city
+ * @property string                         $address
+ * @property string                         $postal_code
+ * @property string                         $launch_date
+ * @property bool                           $active
+ * @property int                            $order
+ * @property string                         $group
+ * @property \Carbon\Carbon                 $created_at
+ * @property \Carbon\Carbon                 $updated_at
+ * @property string                         $deleted_at
+ * @property-read \Rinvex\Country\Country   $country
+ * @property-read \Rinvex\Language\Language $language
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant ordered($direction = 'asc')
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereActive($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereAddress($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereCity($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereCountryCode($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereDeletedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereDescription($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereEmail($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereEmailVerified($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereGroup($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereLanguageCode($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereLaunchDate($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereOrder($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereOwnerId($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant wherePhone($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant wherePhoneVerified($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant wherePostalCode($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereSlug($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereState($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Rinvex\Tenantable\Tenant withGroup($group = null)
+ * @mixin \Eloquent
+ */
 class Tenant extends Model implements Sortable
 {
     use HasSlug;
