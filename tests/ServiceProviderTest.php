5af5efe7e2df2088b7d613fe5a6fcc2e14bb979e
Abdelrahman Omran <me@omranic.com>
2017-07-18 03:36:28 +0200

    Fix deprecated PHPUnit TestCase namespace

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace Rinvex\Tenantable\Test;
 
 use ReflectionClass;
-use PHPUnit_Framework_TestCase;
+use PHPUnit\Framework\TestCase;
 use Illuminate\Container\Container;
 use Illuminate\Support\ServiceProvider;
 use Rinvex\Tenantable\TenantableServiceProvider;
 
-class ServiceProviderTest extends PHPUnit_Framework_TestCase
+class ServiceProviderTest extends TestCase
 {
     /** Get the service provider class. */
     protected function getServiceProviderClass(): string
