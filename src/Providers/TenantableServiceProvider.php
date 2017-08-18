d77ccf31fee27857ef037ca8a5c185d6da13ff90
Abdelrahman Omran <me@omranic.com>
2017-08-18 18:47:08 +0200

    Fix migration path

--- src/Providers/TenantableServiceProvider.php
+++ src/Providers/TenantableServiceProvider.php
@@ -43,7 +43,7 @@ class TenantableServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             // Load migrations
-            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+            $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
 
             // Publish Resources
             $this->publishResources();
