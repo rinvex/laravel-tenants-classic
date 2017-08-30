15bd5f00fbe15fadf92acde47290925674e81fe6
Abdelrahman Omran <me@omranic.com>
2017-08-30 05:18:52 +0200

    Handle default translation through overridden HasTranslations trait

--- src/Models/Tenant.php
+++ src/Models/Tenant.php
@@ -229,30 +229,6 @@ class Tenant extends Model implements TenantContract
     }
 
     /**
-     * Set the translatable name attribute.
-     *
-     * @param string $value
-     *
-     * @return void
-     */
-    public function setNameAttribute($value)
-    {
-        $this->attributes['name'] = json_encode(! is_array($value) ? [app()->getLocale() => $value] : $value);
-    }
-
-    /**
-     * Set the translatable description attribute.
-     *
-     * @param string $value
-     *
-     * @return void
-     */
-    public function setDescriptionAttribute($value)
-    {
-        $this->attributes['description'] = ! empty($value) ? json_encode(! is_array($value) ? [app()->getLocale() => $value] : $value) : null;
-    }
-
-    /**
      * Get the options for generating the slug.
      *
      * @return \Spatie\Sluggable\SlugOptions
