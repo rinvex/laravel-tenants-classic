c7c1ed66608061ed7a80140f9c15c03ff6a1d74a
Abdelrahman Omran <me@omranic.com>
2018-02-02 21:42:43 +0200

    Add force option to artisan commands

--- src/Console/Commands/RollbackCommand.php
+++ src/Console/Commands/RollbackCommand.php
@@ -13,7 +13,7 @@ class RollbackCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'rinvex:rollback:tenants';
+    protected $signature = 'rinvex:rollback:tenants {--force : Force the operation to run when in production.}';
 
     /**
      * The console command description.
@@ -30,6 +30,6 @@ class RollbackCommand extends Command
     public function handle(): void
     {
         $this->warn($this->description);
-        $this->call('migrate:reset', ['--path' => 'vendor/rinvex/tenants/database/migrations']);
+        $this->call('migrate:reset', ['--path' => 'vendor/rinvex/tenants/database/migrations', '--force' => $this->option('force')]);
     }
 }
