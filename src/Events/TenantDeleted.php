3b4f4a64b7d69cdbd905c93f05d8c7fa6c959bfa
Abdelrahman Omran <me@omranic.com>
2020-05-24 05:48:49 +0400

    Specify events queue

--- src/Events/TenantDeleted.php
+++ src/Events/TenantDeleted.php
@@ -15,6 +15,18 @@ class TenantDeleted implements ShouldBroadcast
     use SerializesModels;
     use InteractsWithSockets;
 
+    /**
+     * The name of the queue on which to place the event.
+     *
+     * @var string
+     */
+    public $broadcastQueue = 'events';
+
+    /**
+     * The model instance passed to this event.
+     *
+     * @var \Rinvex\Tenants\Models\Tenant
+     */
     public $tenant;
 
     /**
