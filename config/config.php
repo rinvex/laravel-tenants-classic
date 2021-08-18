dddb19529aec5d64d2acce0c2153220bbdc4ef23
Abdelrahman Omran <me@omranic.com>
2021-08-18 07:42:19 +0400

    Add central domains config options to support additional alias domains

--- config/config.php
+++ config/config.php
@@ -20,4 +20,22 @@ return [
         'tenant' => \Rinvex\Tenants\Models\Tenant::class,
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Central Domains
+    |--------------------------------------------------------------------------
+    |
+    | If your application is accessible through multiple domain names,
+    | you may specify these domains here, so the application router
+    | take care of matching the incoming requests appropriately.
+    |
+    | No need to list the default domain, it is automatically
+    | appended to the compiled list from `app.url` config.
+    |
+    */
+
+    'central_domains' => [
+        'app.rinvex.test',
+    ],
+
 ];
